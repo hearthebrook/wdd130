415a356d79ba43b112817f4d0f6fc5cbce1b2263
Brooklynn <63615317+hearthebrook@users.noreply.github.com>
2022-09-23 08:10:56 -0600

    added files

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # wdd130
 class work from WDD130 Fall 2022
 
+https://hearthebrook.github.io/wdd130/
+
 test
---
+++ aboutme/aboutme.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+const radius = 3;
+let area = 0;
+/* used for debuging*/
+console.long(radius);
+
+area = radius * radius * pi;
+radius = 4;
+area = radius * radius * pi;
