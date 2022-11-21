f60b5afb7c3a99b1d1424def8ac73b294470d740
Brooklynn Hathaway <hearthebrook@gmail.com>
2022-11-21 10:56:32 -0700

    added form function

--- APOD/apod.js
+++ APOD/apod.js
@@ -1,4 +1,4 @@
-const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+const apodUrl = "https://api.nasa.gov/planetary/apod?api_key=0UieBswOrvrViwGPhI6ypg1tdwR9iF8tKV6eT2yJ"
 
 async function getApod(url){
     const results = await fetch(url);
@@ -6,43 +6,56 @@ async function getApod(url){
         const data = await results.json()
         console.log(data)
         insertHtml(data);
+        hideError();
+    }
+    else {
+        showError();
     }
 }
 
 function apodTemplate(data){
     return`
-    <dev id="image">
+    <div id="image">
         <img src=${data.url} alt=${data.title}/>
-    </dev>
-    <dev id="info">
+    </div>
+    <div id="info">
         <h2>${data.title}</h2>
         <h3>${data.date}</h3>
         <p>${data.explanation}</p>
-    </dev>`
+    </div>`
 }
 
 function insertHtml(data) {
+    document.querySelector('#apiPhotoInfo').innerHTML = "";
     const html = apodTemplate(data);
     document.querySelector("#apiPhotoInfo").insertAdjacentHTML('afterbegin', html);
 }
 
-getApod(apodUrl)
+//getApod(apodUrl)
 
 function showError(){
     // select element (qs)
-    const element = document.querySelector("#errorMessage");
+    const element = document.querySelector(".errorMessage");
     // remove hide class 
-    element.remove("#hide");
+    element.classList.remove("hide")
+    //element.remove("#hide");
 }
 
 function hideError(){
     // select element (qs)
-    const element = document.querySelector("#errorMessage");
+    const element = document.querySelector(".errorMessage");
     // add hide class 
-    element.add("#hide");
+    element.classList.add("hide");
 }
 
-showError()
+function getApodByDate(event) {
+    event.preventDefault();
+    let date = document.getElementById("date");
+    let urlByDate = apodUrl + `&date=${date.value}`;
+    console.log('The event listener is working');
+    getApod(urlByDate);
+}
+getApod(apodUrl);
 
-// try except with code 400 
-// display msg in 
+document.querySelector("#getImage").addEventListener('click', getApodByDate)
+
